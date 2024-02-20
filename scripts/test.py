d4b97b8a3c408438a5e2be37e9304481046dd8e3
Mingyo Seo <mingyo@utexas.edu>
2024-02-19 23:46:29 -0600

    Current configuration for gr1 hand movement

--- scripts/test.py
+++ scripts/test.py
@@ -67,8 +67,8 @@ def main(gui, env_type, cam_name="upview", subtask=0, save_video=True):
 
     init_time = env.cur_time
     
-    left_pos =  np.array([.1, 0.5 , .6])
-    right_pos = np.array([.1, -0.5,  .6 ])
+    left_pos =  np.array([.1, 0.5 , .3])
+    right_pos = np.array([.1, -0.5,  .3 ])
     # left_pos =  np.array([-.1, -0.32 , -1.4])
     # right_pos = np.array([-.1, .32, -1.4])
     # target_pos = np.array([0.22,-0.35, -0.1 ])
@@ -84,15 +84,15 @@ def main(gui, env_type, cam_name="upview", subtask=0, save_video=True):
 
         rh_target_pos = right_pos
         lh_target_pos = left_pos
-        # lh_input = geom.euler_to_rot(np.array([0, 0, 0]))
-        # rh_input = geom.euler_to_rot(np.array([0, 0, 0]))
+        lh_input = geom.euler_to_rot(np.array([.7, 0, 0]))
+        rh_input = geom.euler_to_rot(np.array([-.7, 0, 0]))
 
-        # rh_target_rot = np.dot(rh_input, RIGHTFORWARD_GRIPPER)
-        # lh_target_rot = np.dot(lh_input, RIGHTFORWARD_GRIPPER)
+        rh_target_rot = np.dot(rh_input, RIGHTFORWARD_GRIPPER)
+        lh_target_rot = np.dot(lh_input, RIGHTFORWARD_GRIPPER)
         action['trajectory']['left_pos'] = lh_target_pos
         action['trajectory']['right_pos'] = rh_target_pos
-        # action['trajectory']['right_quat'] = geom.rot_to_quat(rh_target_rot)
-        # action['trajectory']['left_quat'] = geom.rot_to_quat(lh_target_rot)
+        action['trajectory']['right_quat'] = geom.rot_to_quat(rh_target_rot)
+        action['trajectory']['left_quat'] = geom.rot_to_quat(lh_target_rot)
         
         # action['trajectory']['left_pos'] = np.array([0.24, -0.34, 0.1])
         # action['trajectory']['right_pos'] = np.array([0.24, 0.34, 0.1])
@@ -102,7 +102,7 @@ def main(gui, env_type, cam_name="upview", subtask=0, save_video=True):
         # ipdb.set_trace()
         env.step(action)
 
-        if env.cur_time > 50.0:
+        if env.cur_time > 4.0:
             done = True
 
     recorder.close()
